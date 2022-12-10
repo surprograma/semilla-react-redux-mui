c71c746ae96956ecb21a09463b004bbf8c8d90f2
Federico Aloi <federico.aloi@gmail.com>
2022-12-09 20:17:19 -0400

    Variable de entorno para la URL de la API

--- src/app/services/baseApi.ts
+++ src/app/services/baseApi.ts
@@ -1,9 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { config } from './config';
 
 // Definimos la API con su configuración base, sin ningún endpoint.
 // Los endpoints serán definidos luego en archivos separados.
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: '//localhost:4000' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: config.apiUrl,
+  }),
   endpoints: () => ({}),
 });
