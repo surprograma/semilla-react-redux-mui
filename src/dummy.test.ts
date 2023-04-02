84840fe57331b6e56b639c412baf27a52594f832
Federico Aloi <federico.aloi@gmail.com>
2023-04-02 16:46:59 -0500

    Test dummy para que pase el CI

---
+++ src/dummy.test.ts
@@ -0,0 +1,9 @@
+// TODO: borrar cuando haya algún test de verdad.
+
+describe('Dummy test', () => {
+  it('está solamente para que no falle el CI', () => {
+    expect(2).toBe(2);
+  });
+});
+
+export {};
